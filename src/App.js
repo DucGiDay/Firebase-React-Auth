02042575ab3fc7486a16584678fee60576ea5a87
TrungDuc <duc123zzmm@gmail.com>
2022-10-16 23:50:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Form from './Components/Common/Form';
+import Home from './Components/Home';
+import {
+  Routes,
+  Route,
+  useNavigate
+} from "react-router-dom";
+import { app } from './firebase-config';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    let authToken = sessionStorage.getItem('Auth Token')
+
+    if (authToken) {
+      navigate('/home')
+    }
+  }, [])
+
+  const handleAction = (id) => {
+    const authentication = getAuth();
+    if (id === 2) {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate('/home')
+          sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken)
+        })
+        .catch((error) => {
+          if(error.code === 'auth/email-already-in-use'){
+            toast.error('Please check the Email');
+          }
+        })
+    }
+    else if (id === 1) {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate('/home')
+          sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken)
+        })
+        .catch((error) => {
+          if(error.code === 'auth/invalid-email'){
+            toast.error('Please check the Email');
+          }
+          if(error.code === 'auth/wrong-password'){
+            toast.error('Please check the Password');
+          }
+        })
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Routes>
+          <Route
+            path='/login'
+            element={
+              <Form
+                title="Login"
+                setEmail={setEmail}
+                setPassword={setPassword}
+                handleAction={() => handleAction(1)}
+              />}
+          />
+          <Route
+            path='/register'
+            element={
+              <Form
+                title="Register"
+                setEmail={setEmail}
+                setPassword={setPassword}
+                handleAction={() => handleAction(2)}
+              />}
+          />
+
+          <Route
+            path='/home'
+            element={
+              <Home />}
+          />
+        </Routes>
+      </>
+      <ToastContainer />
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCufd8TVCH9OWAjMXwlV5eruDoPIM_MbNw",
+  authDomain: "webgolang-174d7.firebaseapp.com",
+  projectId: "webgolang-174d7",
+  storageBucket: "webgolang-174d7.appspot.com",
+  messagingSenderId: "755476210823",
+  appId: "1:755476210823:web:103bdfa52dc118bf8b4422"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
